9fe892bad305b40bcb91b848c3efdd472f8f6730
jisshub <jissmon476@gmial.com>
2019-09-28 00:04:33 +0530

    added new users and saved to users

--- user_reg_proj_root/userapp/urls.py
+++ user_reg_proj_root/userapp/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.register, name='register'),
     path('home/', views.home, name='home'),
-
 ]
+
+
+
+
--- user_reg_proj_root/userapp/views.py
+++ user_reg_proj_root/userapp/views.py
@@ -7,18 +7,18 @@ from django.shortcuts import redirect
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        # if request is post, v create form instance of that posted data
+        # if request is post, v create form instance of UsercreatonFrom and this new form has the posted data.
         form = UserCreationForm(request.POST)
-        # validate that posted form
+        # next checks whether form is valid
         if form.is_valid():
             # if valid grab the posted username from the form.
+            form.save()
             # form.cleaned_data is a dictionary which contains validated datas
             username = form.cleaned_data.get('username')
             # display a flash messages if success,
             messages.success(request, f'Account Created for {username}')
             # redirect to a new page once submited. here home is name of the urlpattern that renders home page.
             return redirect('home')
-
     else:
         # if not post request, create an instance of form
         form = UserCreationForm()
