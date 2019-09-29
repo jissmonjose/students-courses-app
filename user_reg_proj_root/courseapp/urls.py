1257e73833d2fd20a39c2af58c1ff6e06b93e5c6
jisshub <jissmon476@gmial.com>
2019-09-29 12:38:59 +0530

    listed each courses in separated pages

--- user_reg_proj_root/courseapp/urls.py
+++ user_reg_proj_root/courseapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.course_create, name='course_create'),
+    path('courses/', views.course_list, name='course_list'),
+    path('<int:course_id>', views.each_courses, name='each_courses'),
 ]
--- user_reg_proj_root/courseapp/views.py
+++ user_reg_proj_root/courseapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .course_form import CourseForm
 from django.shortcuts import redirect, get_object_or_404
 from .models import CourseModel
+from django.contrib import messages
 
 
 # Create your views here.
@@ -9,10 +10,18 @@ def course_create(request, template_name='courseapp/course.html'):
     form = CourseForm(request.POST or None)
     if form.is_valid():
         form.save()
+        messages.success(request, 'Cousrse successfully creatad')
         return redirect('course_create')
     return render(request, template_name, {'form': form})
 
 
-def course_read(request, pk, template_name='courseapp/courselist.html'):
-    course = get_object_or_404(CourseModel, pk=pk)
-    return render(request, template_name, {'course': course})
+# course lsist
+def course_list(request, template_name='courseapp/courselist.html'):
+    courses = CourseModel.objects.all().order_by('name')
+    return render(request, template_name, {'course': courses})
+
+
+# each courses
+def each_courses(request, course_id, template_name='courseapp/course_details.html'):
+    each_one = CourseModel.objects.get(id=course_id)
+    return render(request, template_name, {'each_one': each_one})
