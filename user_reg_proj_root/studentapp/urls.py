93d88ff4092671ee9aaa405bcd510ccd5263b924
jisshub <jissmon476@gmial.com>
2019-10-03 16:41:01 +0530

    added static files

--- user_reg_proj_root/studentapp/urls.py
+++ user_reg_proj_root/studentapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import student_register
+from .views import student_register, course_view
 
 urlpatterns = [
-    path('', student_register, name='student_register')
+    path('', student_register, name='student_register'),
+    path('view/', course_view, name='course_view'),
 ]
--- user_reg_proj_root/studentapp/views.py
+++ user_reg_proj_root/studentapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.forms import ModelForm
 from .models import Student
+from courseapp.models import CourseModel
+from django.core.paginator import Paginator
 
 
 class StudentForm(ModelForm):
@@ -17,3 +19,21 @@ def student_register(request, template_name='studentapp/apply.html'):
         form.save()
         return redirect('student_register')
     return render(request, template_name, {'form': form})
+
+
+def course_view(request, template_name='studentapp/courses.html'):
+    courses = CourseModel.objects.order_by('name')
+    paginator = Paginator(courses, 3)
+    page = request.GET.get('page')
+    page_courses = paginator.get_page(page)
+
+    context = {
+        'courses': page_courses
+    }
+
+    return render(request, template_name, context)
+
+
+def each_course(request, template_name='studentapp/'):
+    pass
+
