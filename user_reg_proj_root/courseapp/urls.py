f082ec1b8ee48ac2ebcb4f6f55c1a5934922f4aa
jisshub <jissmon476@gmial.com>
2019-10-21 14:15:42 +0530

    error solved now committed

--- user_reg_proj_root/courseapp/urls.py
+++ user_reg_proj_root/courseapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.course_create, name='course_create'),
-    path('courses/', views.course_list, name='course_list'),
+    path('course/', views.course_list, name='course_list'),
     path('<int:pk>', views.each_courses, name='each_courses'),
     path('delete/<int:pk>', views.CourseDelete.as_view(), name='course_delete'),
     path('edit/<int:pk>', views.CourseEdit.as_view(), name='course_edit'),
